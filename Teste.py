7aa1bd5dc60fcb4e9a7174a11ede5c1845a360b0
Rafael Pereira <rafaelrps11@gmail.com>
2017-12-27 23:18:33 -0200

    Faz ajustes na classe de testes

--- Teste.py
+++ Teste.py
@@ -3,9 +3,10 @@
 
 import timeit
 from leitor import Leitor
-from MongoDbDAO import MongoDbDAO
-from RedisDbDAO import RedisDbDAO
-
+from MongodbDAO import MongodbDAO
+from RedisDAO import RedisDAO
+from CassandraDAO import CassandraDAO
+from Neo4jDAO import Neo4jDAO
 import sys
 
 class Teste:
@@ -24,21 +25,46 @@ class Teste:
 	python Teste.py -m
 	'''
 
+	def _mensagem(self, banco, tempo, operacao, quantidade):
+		print "O %s demorou %f segundos para %s %d documentos " % (banco, tempo, operacao, quantidade)
+
+	def _getTimeDifference(self, init_time):
+		final_time = timeit.default_timer()
+		return final_time - init_time
+
 	def getObjectsFromFile(self, arquivo):
 		leitor = Leitor()
 		objetos = leitor.retorna_lista_de_objetos(arquivo)
 		return objetos
 	
 	def insertObjecbyObject(self, objetos):
-
 		init_time = timeit.default_timer()
 		for objeto in objetos:
 			self.classeDAO.insertObject(objeto)
-		print 'O %s levou %f segundos para inserir %d documentos' % (self.banco, self._getTimeDifference(init_time), len(objetos))
+		self._mensagem(self.classeDAO.db_name, self._getTimeDifference(init_time), 'INSERIR', len(objetos))
+
+	def inserGroupOfObjects(self, objetos):
+		pass
 
 	def getAllObjects(self):
 		init_time = timeit.default_timer()
-		print 'O %s demorou %f segundos para buscar todos objetos' % (self.banco, self._getTimeDifference(init_time))
+		documentos = self.classeDAO.getAllObjects()
+		self._mensagem(self.classeDAO.db_name, self._getTimeDifference(init_time), 'RECEBER TODOS', documentos.count())
+
+	def getByCivilStatus(self, civil_status):
+		init_time = timeit.default_timer()
+		quantidade = self.classeDAO.getByCivilStatus(civil_status)
+		self._mensagem(self.classeDAO.db_name, self._getTimeDifference(init_time), 'BUSCAR por estado civil de solteiro, e foram encontrados ', quantidade)
+
+	def updateCivilStatus(self, old_status, new_status):
+		init_time = timeit.default_timer()
+		quantidade = self.classeDAO.updateCivilStatus(old_status, new_status)
+		self._mensagem(self.classeDAO.db_name, self._getTimeDifference(init_time), 'ATUALIZAR O ESTADO CIVIL DE ', quantidade)
+
+	def deleteByCivilStatus(self, civil_status_to_delete):
+		init_time = timeit.default_timer()
+		quantidade = self.classeDAO.deleteByCivilStatus(civil_status_to_delete)
+		self._mensagem(self.classeDAO.db_name, self._getTimeDifference(init_time), 'DELETAR', quantidade)
 
 	def searchObjectsByCity(self, city):
 		init_time = timeit.default_timer()
@@ -55,30 +81,48 @@ class Teste:
 
 	def getQuantityOfValuesOnDB(self):
 		init_time = timeit.default_timer()
-		self.classeDAO.getQuantityObjects()
+		self.classeDAO.getQuantityOfValuesOnDB()
 
-	def _getTimeDifference(self, init_time):
-		final_time = timeit.default_timer()
-		return final_time - init_time
+	def deleteAllObjects(self):
+		self.classeDAO.deleteAllObjects()
 
 	def __init__(self):
 		if sys.argv[1] == 'cassandra' or sys.argv[1] == '-c':
 			self.banco = 'Cassandra'
-		if sys.argv[1] == 'mongodb' or sys.argv[1] == '-m':
-			self.classeDAO = MongoDbDAO()
+			self.classeDAO = CassandraAO()
+		elif sys.argv[1] == 'mongodb' or sys.argv[1] == '-m':
+			self.classeDAO = MongodbDAO()
 			self.banco = 'MongoDB'
-		if sys.argv[1] == 'neo4j' or sys.argv[1] == '-n':
+		elif sys.argv[1] == 'neo4j' or sys.argv[1] == '-n':
 			self.banco = 'Neo4j'
-		if sys.argv[1] == 'redis' or sys.argv[1] == '-r':
-			self.classeDAO = RedisDbDAO()
+			self.classeDAO = Neo4jDAO()
+		elif sys.argv[1] == 'redis' or sys.argv[1] == '-r':
+			self.classeDAO = RedisDAO()
 			self.banco = 'Redis'
 		else:
-			self.classeDAO = MongoDbDAO()
+			self.classeDAO = MongodbDAO()
 			self.banco = 'MongoDB'
+
+		if sys.argv[2] == '1':
+			self.carga = 'carga/1mil.txt'
+		elif sys.argv[2] == '10':
+			self.carga = 'carga/10mil.txt'
+		elif str(sys.argv[2]) == '50':
+			self.carga = 'carga/50mil.txt'
+		elif sys.argv[2] == '100':
+			self.carga = 'carga/100mil.txt'
+		else:
+			self.carga = 'carga/1mil.txt'
+
 
 
 teste = Teste()
-objetos = teste.getObjectsFromFile('milhao.txt')
+print teste.carga
+objetos = teste.getObjectsFromFile(teste.carga)
 teste.insertObjecbyObject(objetos)
 teste.getQuantityOfValuesOnDB()
-teste.searchObjectsByCity('brasilia')
+teste.getAllObjects()
+teste.getByCivilStatus('Solteiro')
+teste.updateCivilStatus('Solteiro', 'Alterado')
+teste.deleteByCivilStatus('Alterado')
+teste.deleteAllObjects()
