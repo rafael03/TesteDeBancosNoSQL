ef0c4cd0f66187b4587f5303de4545544d165722
Rafael Pereira <rafaelrps11@gmail.com>
2017-12-27 23:15:22 -0200

    Finaliza a implementação dos testes no MongoDB

--- leitor.py
+++ leitor.py
@@ -13,16 +13,34 @@ class Leitor:
 		linhas = arquivo.readlines()
 		return linhas
 
-	# def separa_linhas_por_virgula(self, linhas):
-	# 	objeto = linhas[0].split(',')
-	# 	return objeto
-
 	def transforma_cada_linha_em_um_objeto(self, linhas):
 		lista = []
 		for linha in linhas:
+			linha = linha.replace('\"','')
 			objeto = linha.split(',')
-			lista.append({"nome":objeto[0],
-						 "idade": objeto[1]})
+
+			lista.append({"ANO_NASCIMENTO": objeto[0],
+							"PESO": objeto[1],
+							"ALTURA": objeto[2],
+							"CABECA": objeto[3],
+							"CALCADO": objeto[4],
+							"CINTURA": objeto[5],
+							"RELIGIAO": objeto[6],
+							"MUN_NASCIMENTO": objeto[7],
+							"UF_NASCIMENTO": objeto[8],
+							"PAIS_NASCIMENTO": objeto[9],
+							"ESTADO_CIVIL": objeto[10],
+							"SEXO": objeto[11],
+							"ESCOLARIDADE": objeto[12],
+							"VINCULACAO_ANO": objeto[13],
+							"DISPENSA": objeto[14],
+							"ZONA_RESIDENCIAL": objeto[15],
+							"MUN_RESIDENCIA": objeto[16],
+							"UF_RESIDENCIA": objeto[17],
+							"PAIS_RESIDENCIA": objeto[18],
+							"JSM": objeto[19],
+							"MUN_JSM": objeto[20],
+							"UF_JSM": objeto[21]})
 		return lista
 
 	def retorna_lista_de_objetos(self, arquivo):
