db6e13ae727da1e07f4c2ed7e30cad26dea90928
Rafael Pereira <rafaelrps11@gmail.com>
2018-01-06 23:04:45 -0200

    Apaga da listsa o que já foi utilizado para liberar espaço na memória

--- RedisDAO.py
+++ RedisDAO.py
@@ -17,12 +17,13 @@ class RedisDAO:
 	def insertGroupOfObjects(self, objetos):
 		values = []
 		objetos_parts = [objetos[i:i+100] for i in range(0, len(objetos), 100)]
-		for obj in objetos_parts:
+		for obj in objetos_parts[:]:
 			pipe = self.redis_db.pipeline()
 			for objeto in obj:
 				id_ = uuid.uuid4().hex
 				pipe.set(str(id_), objeto)
 			values = pipe.execute()
+			objetos_parts.remove(obj)
 
 	def getAllObjects(self):
 		keys = self.redis_db.keys()
@@ -37,28 +38,54 @@ class RedisDAO:
 	def getByCivilStatus(self, civil_status):
 		object_dict = {}
 		objects, quantity = self.getAllObjects()
-		for objeto in objects:
-			obj = ast.literal_eval(objeto)
-			if obj['ESTADO_CIVIL'] == civil_status:
-				object_dict[obj['id_']] = obj
+		objetos_parts = [objects[i:i+200] for i in range(0, len(objects), 200)]
+		del objects
+
+		for parts in objetos_parts[:]:
+			for objeto in parts:
+				obj = ast.literal_eval(objeto)
+				if obj['ESTADO_CIVIL'] == civil_status:
+					object_dict[obj['id_']] = obj
+			objetos_parts.remove(parts)
 		return object_dict, len(object_dict)
 
 	def updateCivilStatus(self, old_status, new_status):
 		updated_objects = {}
-		list_id, quantity = self.getByCivilStatus(old_status)
-		for id_objeto in list_id:
-			object_to_update = self.redis_db.get(id_objeto)
-			object_dict = ast.literal_eval(object_to_update)
-			object_dict['ESTADO_CIVIL'] = new_status
-			self.redis_db.set(id_objeto, object_dict)
-			updated_objects[id_objeto] = object_dict
+		ids_list = []
+		objects_dict, quantity = self.getByCivilStatus(old_status)
+		for ids in objects_dict:
+			ids_list.append(ids)
+		del objects_dict
+
+		ids_in_parts = [ids_list[i:i+200] for i in range(0, len(ids_list), 200)]
+		del ids_list
+
+		for parts_from_ids in ids_in_parts[:]:
+			for id_objeto in parts_from_ids:
+				object_to_update = self.redis_db.get(id_objeto)
+				object_dict = ast.literal_eval(object_to_update)
+				object_dict['ESTADO_CIVIL'] = new_status
+				self.redis_db.set(id_objeto, object_dict)
+				updated_objects[id_objeto] = object_dict
+			ids_in_parts.remove(parts_from_ids)
 		return updated_objects, len(updated_objects)
 
 
 	def deleteByCivilStatus(self, civil_status_to_delete):
-		list_id, quantity = self.getByCivilStatus(civil_status_to_delete)
-		self.redis_db.delete(list_id)
-		return list_id, len(list_id)
+		ids_list = []
+		objects_dict, quantity = self.getByCivilStatus(civil_status_to_delete)
+		for ids in objects_dict:
+			ids_list.append(ids)
+		del objects_dict
+
+		ids_in_parts = [ids_list[i:i+200] for i in range(0, len(ids_list), 200)]
+		del ids_list
+
+		for parts_from_ids in ids_in_parts[:]:
+			for id_objeto in parts_from_ids:
+				self.redis_db.delete(id_objeto)
+			ids_in_parts.remove(parts_from_ids)
+		return {}, quantity
 
 	def deleteAllObjects(self):
 		self.redis_db.flushall()
